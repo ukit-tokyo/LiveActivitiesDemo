7fa212d3167bd4e4e31cb6b5980faaa350a34d3d
ukit-tokyo <t.yuki@ukit.tokyo>
2025-11-11 22:58:48 +0900

    setup widget extension

---
+++ SampleLiveActivities/SampleLiveActivitiesBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLiveActivitiesBundle.swift
+//  SampleLiveActivities
+//
+//  Created by Taichi Yuki on 2025/11/11.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SampleLiveActivitiesBundle: WidgetBundle {
+    var body: some Widget {
+        SampleLiveActivities()
+        SampleLiveActivitiesLiveActivity()
+    }
+}
